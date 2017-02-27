8d1c06de2b6c4dc928fb1f7875ad157b5fe412b1
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-02-27 21:47:18 +0900

    fix

--- main.go
+++ main.go
@@ -41,8 +41,9 @@ func main() {
 		for _, event := range events {
 			log.Println("type", event.Type)
 			if event.Type == linebot.EventTypeBeacon {
-				//log.Println("type", event.Type)
+				
 				switch message := event.Message.(type) {
+					log.Println("message", message)
 				case *linebot.TextMessage:
 					log.Println("message", message)
 					log.Println("linebot.NewTextMessage(message.Text)", linebot.NewTextMessage(message.Text))
